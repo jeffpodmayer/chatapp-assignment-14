7627aac6a2c27da424db7a21222233b27ab27d4e
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-02-24 11:43:40 -0800

    created general JS page

--- src/main/java/com/coderscampus/chatapp/a14/web/ChannelController.java
+++ src/main/java/com/coderscampus/chatapp/a14/web/ChannelController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.coderscampus.chatapp.a14.domain.Channel;
-import com.coderscampus.chatapp.a14.domain.User;
 import com.coderscampus.chatapp.a14.service.ChannelService;
 import com.coderscampus.chatapp.a14.service.UserService;
 
@@ -20,12 +18,10 @@ public class ChannelController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/channel/1")
-	public String getChannelByChannelId(ModelMap model, @PathVariable Long channelId) {
-		User user = userService.findbyUserId(userId);
+	@GetMapping("/channel/general")
+	public String getGeneralChannel(ModelMap model) {
 		Channel channel = new Channel();
 		model.put("channel", channel);
-		model.put("user", user);
-		return "channel/" + channel.getChannelId();
+		return "general-channel";
 	}
 }
--- src/main/java/com/coderscampus/chatapp/a14/web/UserController.java
+++ src/main/java/com/coderscampus/chatapp/a14/web/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.coderscampus.chatapp.a14.domain.Channel;
 import com.coderscampus.chatapp.a14.domain.User;
 import com.coderscampus.chatapp.a14.service.UserService;
 
