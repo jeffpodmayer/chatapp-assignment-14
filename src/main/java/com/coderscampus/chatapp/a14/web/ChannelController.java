d01e352a997bd37b61ded1f02ed4f908f2800d66
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-02-29 11:42:39 -0800

    Update ChannelController.java

--- src/main/java/com/coderscampus/chatapp/a14/web/ChannelController.java
+++ src/main/java/com/coderscampus/chatapp/a14/web/ChannelController.java
@@ -1,6 +1,5 @@
 package com.coderscampus.chatapp.a14.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +16,12 @@ import com.coderscampus.chatapp.a14.domain.Message;
 import com.coderscampus.chatapp.a14.domain.User;
 import com.coderscampus.chatapp.a14.service.ChannelService;
 import com.coderscampus.chatapp.a14.service.MessageService;
-import com.coderscampus.chatapp.a14.service.UserService;
 
 @Controller
 public class ChannelController {
 
 	@Autowired
 	private ChannelService channelService;
-
-	@Autowired
-	private UserService userService;
 
 	@Autowired
 	private MessageService messageService;
@@ -43,50 +38,25 @@ public class ChannelController {
 	public String viewChannelByChannelId(@PathVariable Long channelId, ModelMap model) {
 		Channel channel = channelService.findbyChannelId(channelId);
 		model.put("channel", channel);
-		// System.out.println(channel);
+		System.out.println("You are on channel:" + channel);
 		return "channel";
 	}
 
 	@PostMapping("/joinChannel/{channelId}")
 	@ResponseBody
 	public Channel joinChannel(@RequestBody Channel channelData, @PathVariable Long channelId) {
-		Channel channel = channelData;
+		Channel channel = channelService.findbyChannelId(channelId);
 		List<User> username = channelData.getUsers();
 		
 		if (channel == null) {
 			System.out.println(channel);
 		}
-
-//		User user = new User();
-//		user.setUserId(channelData.getUsers().get(0).getUserId());
-//		user.setUsername(channelData.getUsers().get(0).getUsername());
-//
-//		channel.getUsers().add(user);
-//		channelService.saveChannel(channel);
-
+		
 		System.out.println("Joined channel: " + channel);
 		System.out.println("Channel username is: " + username);
 
 		return channel;
 	}
-
-//	@PostMapping("/joinChannel/{channelId}")
-//	@ResponseBody
-//	public Channel joinChannel(@PathVariable Long channelId, @PathVariable User username, ModelMap model) {
-//		Channel channel = channelService.findbyChannelId(channelId);
-//		User currentUser = userService.findByUsername(username);
-//		if (channel == null) {
-//			channel = new Channel();
-//			channelService.saveChannel(channel);
-//		}
-//
-//		model.put("username", currentUser);
-//
-//		System.out.println("Joined channel: " + channel);
-//		System.out.println("Channel username is: " + username);
-//
-//		return channel;
-//	}
 
 	@GetMapping("/channel/getNewMessages/{channelId}")
 	public List<Message> getNewMessagesForChannel(@PathVariable Long channelId) {
