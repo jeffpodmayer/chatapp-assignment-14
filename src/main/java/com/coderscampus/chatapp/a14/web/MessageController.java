cfa0b42ca59fb7d1a1942503eac44bfc1054b7b0
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-03-03 13:22:49 -0800

    Update MessageController.java

--- src/main/java/com/coderscampus/chatapp/a14/web/MessageController.java
+++ src/main/java/com/coderscampus/chatapp/a14/web/MessageController.java
@@ -40,7 +40,7 @@ public class MessageController {
 	public List<Message> getNewMessagesForChannel(@PathVariable Long channelId) {
 		Channel channel = channelService.findByChannelId(channelId);
 		List<Message> messages = channel.getMessages();
-		messages.sort(Comparator.comparing(Message::getTimestamp).reversed());  
+//		messages.sort(Comparator.comparing(Message::getTimestamp).reversed());  
 		return messages;
 				
 	}
